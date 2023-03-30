ca8eface16eabce6faf0350aacf8e0e997b8f19d
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-03-30 14:31:09 +0700

    проверяем тесты

--- controllers/cards.js
+++ controllers/cards.js
@@ -33,8 +33,7 @@ module.exports.deleteCard = (req, res) => {
     .catch((err) => {
       if (req.params.cardId.length != 25) {
         res.status(400).send({ message: "Не правильный ID" });
-      }
-      if (err.name === "CastError") {
+      } else if (err.name === "CastError") {
         res.status(404).send({ message: "Карточка не найдена" });
       } else {
         res.status(500).send({ message: "Произошла ошибка" });
--- controllers/users.js
+++ controllers/users.js
@@ -23,16 +23,18 @@ module.exports.getUsers = (req, res) => {
 module.exports.getUserId = (req, res) => {
   User.findById(req.params.userId)
     .then((user) => {
-      if (user == null) {
-        res.status(404).send({ message: "Пользователь не найден" });
-      }
+      // if (user == null) {
+      //   res.status(404).send({ message: "Пользователь не найден" });
+      // }
       res.send({ data: user });
     })
     .catch((err) => {
+      // if (req.params.userId.length != 25) {
+      //   res.status(400).send({ message: "Не правильный ID" });
+      // }
       if (err.name === "ValidationError") {
         res.status(400).send({ message: "Переданы некорректные данные" });
-      }
-      if (err.name === "CastError") {
+      } else if (err.name === "CastError") {
         res.status(404).send({ message: "Пользователь не найден" });
       } else {
         res.status(500).send({ message: "Произошла ошибка" });
@@ -52,8 +54,7 @@ module.exports.updateProfile = (req, res) => {
     .catch((err) => {
       if (err.name === "ValidationError") {
         res.status(400).send({ message: "Переданы некорректные данные" });
-      }
-      if (err.name === "CastError") {
+      } else if (err.name === "CastError") {
         res.status(404).send({ message: "Пользователь не найден" });
       } else {
         res.status(500).send({ err });
@@ -71,8 +72,7 @@ module.exports.updateAvatar = (req, res) => {
     .catch((err) => {
       if (err.name === "ValidationError") {
         res.status(400).send({ message: "Переданы некорректные данные" });
-      }
-      if (err.name === "CastError") {
+      } else if (err.name === "CastError") {
         res.status(404).send({ message: "Пользователь не найден" });
       } else {
         res.status(500).send({ message: "Произошла ошибка" });
