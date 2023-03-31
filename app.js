f28ad88700b2d6c6fc68c1cd93c49c869b29e9ef
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-03-31 20:48:09 +0700

    kkk

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 
 const app = express();
@@ -20,4 +21,7 @@ app.use('/users', require('./routes/users'));
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Адрес не существует' });
+});
+app.listen(PORT, () => {
+  console.log('Privetiki :)');
 });
