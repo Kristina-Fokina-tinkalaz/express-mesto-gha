8cde17aabf9f17d929a3905de01e0d442b6af973
Kristina Fokina <kristinalazareva1993@yandex.ru>
2023-03-31 19:52:48 +0700

    lint должен работать

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,37 +4,12 @@ module.exports = {
         "commonjs": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:vue/vue3-essential"
-    ],
     "overrides": [
     ],
     "parserOptions": {
         "ecmaVersion": "latest"
     },
-    "plugins": [
-        "vue"
-    ],
     "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-underscore-dangle": [
-          "error",{"allow":["_id"]}
-        ]
+      "no-underscore-dangle": ["error",{"allow":["_id"]}]
     }
 }
